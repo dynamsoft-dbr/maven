2792d8dc4024665e06527a907ce02fdb156125eb
Xiao Ling <lingxiao1002@gmail.com>
2018-07-11 11:30:48 +0800

    Update

--- BarcodeReaderDemo/src/main/java/com/dynamsoft/demo/App.java
+++ BarcodeReaderDemo/src/main/java/com/dynamsoft/demo/App.java
@@ -1,13 +1,26 @@
 package com.dynamsoft.demo;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.dynamsoft.barcode.jni.BarcodeReader;
+import com.dynamsoft.barcode.jni.TextResult;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class App {
+    public static void main(String[] args) {
+        String path = System.getProperty("user.dir");
+        Path p = Paths.get(path, "images", "test.jpg");
+        System.out.println("image: " + p);
+        try {
+            BarcodeReader reader = new BarcodeReader(
+                    "t0068MgAAAEEC0tQ7CbKWKyywjkX97zQBIltf52LRibwf0AJLBuzvNuCzxE1bVv6pT2D6GelDmOv0f6+IUE1NvvBWmkgSNXE=");
+            TextResult[] results = reader.decodeFile(p.toString(), "");
+            for (TextResult result : results) {
+                System.out.println(result.barcodeText);
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println(e);
+        }
+
     }
 }
