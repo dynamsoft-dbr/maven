c63921492457be7b923fe11958e0de10be7fd867
Xiao Ling <lingxiao1002@gmail.com>
2018-09-11 14:10:29 +0800

    Update

--- BarcodeReaderDemo/src/main/java/com/dynamsoft/demo/App.java
+++ BarcodeReaderDemo/src/main/java/com/dynamsoft/demo/App.java
@@ -6,13 +6,35 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class App {
+    private static String OS = System.getProperty("os.name").toLowerCase();
+
+    public static boolean isWindows() {
+        return (OS.indexOf("win") >= 0);
+    }
+
+    public static boolean isMac() {
+        return (OS.indexOf("mac") >= 0);
+    }
+
+    public static boolean isLinux() {
+        return (OS.indexOf("nux") >= 0);
+    }
+
     public static void main(String[] args) {
         String path = System.getProperty("user.dir");
-        Path p = Paths.get(path, "images", "test.jpg");
+        Path p = Paths.get(path, "../images", "test.jpg");
         System.out.println("image: " + p);
+        System.out.println(OS);
+        String license = null;
+        if (isWindows()) {
+            license = "t0068NQAAAGWe/zXkYmggvyFrd8PmfjplKakH67Upt9HvuRDIBAV6MZ4uODuL1ZUgSEAOygejsfwj6XRKI5iD1tLKZBRGo2c=";
+        }
+        else if(isLinux()) {
+            license = "t0068NQAAAIY/7KegDlZn7YiPdAj0cbA11n2CwuCEWnk2KYla55ozdfmoasjRIpHhl0EUZmko/zxfxFLH3FpLw694uihoCVM=";
+        }
+
         try {
-            BarcodeReader reader = new BarcodeReader(
-                    "t0068MgAAAEEC0tQ7CbKWKyywjkX97zQBIltf52LRibwf0AJLBuzvNuCzxE1bVv6pT2D6GelDmOv0f6+IUE1NvvBWmkgSNXE=");
+            BarcodeReader reader = new BarcodeReader(license);
             TextResult[] results = reader.decodeFile(p.toString(), "");
             for (TextResult result : results) {
                 System.out.println(result.barcodeText);
--- README.md
+++ README.md
@@ -10,4 +10,9 @@ Use Visual Studio Code and Maven to build and run Java barcode reader built with
 ## HowTo
 1. Load projects to Visual Studio Code.
 2. Run `mvn package`.
-3. Press `F5` to debug the code.
+3. Press `F5` to debug the code. Or run it in command line:
+    
+    Windows
+    ```
+    java -cp target/BarcodeReaderDemo-1.0-SNAPSHOT.jar;%USERPROFILE%\.m2\repository\com\dynamsoft\dbr\6.2\dbr-6.2.jar  com.dynamsoft.demo.App
+    ```
